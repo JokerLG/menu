261ecc3f7f0d9e7ec9b071ecad82f393b2f8b3a9
Joker <1271541514@qq.com>
2025-12-22 16:19:13 +0800

    初始化项目

---
+++ src/test/java/com/lg/menu/MenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lg.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
